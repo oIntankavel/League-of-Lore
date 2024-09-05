f5e7e3c4e69ce8f21d1b0bcbd7547e5bd8865701
oIntankavel <165325457+oIntankavel@users.noreply.github.com>
2024-09-05 08:39:11 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        'titulo': 'yasuo',
+        'descricao': 'Yasuo, um espadachim ioniano orgulhoso e habilidoso, carrega o peso de uma acusação injusta. Na juventude, ele foi acusado de assassinar seu mestre e, incapaz de provar sua inocência, viu-se forçado a fugir, tornando-se um exilado em sua própria terra. Determinado a limpar seu nome e encontrar o verdadeiro assassino, Yasuo vaga por Ionia, sua espada e sua maestria sobre o vento suas únicas companhias. Ele busca redenção, mas também luta contra seus próprios demônios internos, como a impaciência e a arrogância que o levaram a tomar decisões impulsivas no passado.',
+        'link': 'https://universe.leagueoflegends.com/pt_BR/story/champion/yasuo'
+    },
+    {
+        'titulo': 'Yone',
+        'descricao': 'Yone, o irmão mais velho de Yasuo, foi morto pelo próprio irmão em um trágico duelo. Agora, ele vagueia pelo reino espiritual, caçando os espíritos malignos conhecidos como azakana. Yone busca redenção e paz, mas também luta contra a sombra de seu passado e a culpa que carrega por sua morte.',
+        'link': 'https://universe.leagueoflegends.com/pt_BR/story/champion/yone/'
+    },
+    {
+        'titulo': 'Riven',
+        'descricao': 'Riven, uma ex-soldado noxiana, carrega o fardo de seu passado brutal e da destruição que causou em Ionia. Desiludida com o império noxiano, ela busca redenção e um novo propósito, lutando para encontrar seu lugar em um mundo que ela ajudou a destruir.',
+        'link': 'https://universe.leagueoflegends.com/pt_BR/story/champion/riven/'
+    }
+];
